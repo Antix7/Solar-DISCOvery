3e30013b2d742a4491965b55d2232b389ba9827a
filonfighter <cameraatliberty@gmail.com>
2023-10-07 12:08:32 +0200

    Added reading single file ability

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+
+data = pd.read_csv(
+    "dsc_fc_summed_spectra_2023_v01.csv",
+     delimiter = ',', 
+     parse_dates=[0], 
+     na_values='0', 
+     header = None
+)
+
