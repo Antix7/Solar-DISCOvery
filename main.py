e8592ee85852b3b4b7824f061d89e29a1f3c776d
filonfighter <cameraatliberty@gmail.com>
2023-10-07 12:39:35 +0200

    Added column labeling and rows indexing

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-data = pd.read_csv(
+##Reads CSV
+df = pd.read_csv(
     "dsc_fc_summed_spectra_2023_v01.csv",
      delimiter = ',', 
      parse_dates=[0], 
@@ -12,3 +12,13 @@ data = pd.read_csv(
      header = None
 )
 
+##Sets rows index to 'Datetime_UTC'
+df.set_index(0, inplace=True)
+df.index.name = 'Datetime_UTC'
+
+##Sets columns indexes
+magnetic_field_cols = ["Magnetic_Field_GSE_X", "Magnetic_Field_GSE_Y", "Magnetic_Field_GSE_Z"]
+spectrum_cols = [f"Raw_Spectrum_{i}" for i in range(1, 51)]
+df.columns = magnetic_field_cols + spectrum_cols
+
+print(df.head())
