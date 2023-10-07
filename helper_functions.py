1fcec2a235185343498e11d80570234b0d7d5ded
Antix7 <antekklejman@gmail.com>
2023-10-07 15:42:45 +0200

    Implemented calculating Bz parameter

--- helper_functions.py
+++ helper_functions.py
@@ -7,6 +7,8 @@ def compute_dipole_tilt_angle(target_date):
     """
     Compute the dipole tilt angle for a target date.
     """
+    # Make sure the target date is a datetime.date object
+    target_date = dt.date(target_date.year, target_date.month, target_date.day)
 
     vernal_equinox = dt.date(2015, 3, 20)  # Vernal equinox
     days_in_year = 365.25  # Average number of days in a year
@@ -24,13 +26,15 @@ def gse_to_gsm(vector_gse, date):
     - vector_gse (tuple or list): Vector in GSE coordinates (Bx, By, Bz)
     - date (date)
     """
-    dipole_tilt_angle = compute_dipole_tilt_angle(date)
+    dipole_tilt_angle = compute_dipole_tilt_angle(date) / 180 * math.pi
 
     rotation_matrix = np.array([
         [1, 0, 0],
         [0, np.cos(dipole_tilt_angle), -np.sin(dipole_tilt_angle)],
         [0, np.sin(dipole_tilt_angle), np.cos(dipole_tilt_angle)]
     ])
+    # invert the rotation matrix
+    rotation_matrix = np.linalg.inv(rotation_matrix)
 
     return tuple(np.matmul(rotation_matrix, vector_gse))
 
--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import datetime as dt
+from helper_functions import *
 
 # conversion of .csv files to pandas dataframe and making one big dataframe of all datasets
 df = pd.DataFrame()
-for i in range(16, 24):
+for i in range(16, 17):
     print("Reading dsc_fc_summed_spectra_20{}_v01.csv".format(i))
     current_df = pd.read_csv(
         "data/dsc_fc_summed_spectra_20{}_v01.csv".format(i),
@@ -29,5 +31,18 @@ magnetic_field_cols = ["Magnetic_Field_GSE_X", "Magnetic_Field_GSE_Y", "Magnetic
 spectrum_cols = [f"Raw_Spectrum_{i}" for i in range(1, 51)]
 df.columns = magnetic_field_cols + spectrum_cols
 
-print(df.head())
-print(df.shape)
+Bz = np.array([])
+date = dt.datetime(2016, 7, 1, 0, 0)
+while date < dt.datetime(2016, 8, 1, 0, 0):
+    Bz = np.append(Bz, gse_to_gsm(
+        (df.loc[date, 'Magnetic_Field_GSE_X'],
+         df.loc[date, 'Magnetic_Field_GSE_Y'],
+         df.loc[date, 'Magnetic_Field_GSE_Z']),
+        date))
+    date += dt.timedelta(minutes=1)
+
+# take a walking average over a window of 1 hour
+Bz = np.convolve(Bz, np.ones(60)/60, mode='same')
+
+plt.plot(Bz)
+plt.show()
