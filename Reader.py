44fe79e6befe2c4407b3683334c38404d65b23e2
filonfighter <cameraatliberty@gmail.com>
2023-10-08 06:49:49 +0200

    i dont even bother

--- Reader.py
+++ Reader.py
@@ -4,7 +4,10 @@ import io
 with pd.HDFStore('dataframes.h5') as store:
     dfs_from_h5 = [store[key] for key in store.keys()]
 
+# Sort dfs based on their size (max size first)
+dfs_from_h5_sorted = sorted(dfs_from_h5, key=len, reverse=True)
+
 # Check results
-for df in dfs_from_h5:
+for df in dfs_from_h5_sorted:
     print(df)
     print("-" * 50)
